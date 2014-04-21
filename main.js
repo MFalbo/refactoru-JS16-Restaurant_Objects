45f13fe65fdc23082776124a8f16c5f1afffa3d5
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-21 17:27:00 -0600

    Part 2. Need to complete toString functionality

--- main.js
+++ main.js
@@ -29,13 +29,13 @@ var FoodItem = function(name, calories, vegan, glutenFree, citrusFree){
 };
 
 
-var pizza = new FoodItem('Pizza', 700, false, false, true);
-var sandwich = new FoodItem('Sandwich', 350, true, false, true);
-var soup = new FoodItem('Soup', 225, true, true, false);
+// var pizza = new FoodItem('Pizza', 700, false, false, true);
+// var sandwich = new FoodItem('Sandwich', 350, true, false, true);
+// var soup = new FoodItem('Soup', 225, true, true, false);
 
-console.log(pizza.toString());
-console.log(sandwich.toString());
-console.log(soup.toString());
+// console.log(pizza.toString());
+// console.log(sandwich.toString());
+// console.log(soup.toString());
 
 
 // -----------------------------------PART2---------------------------------
@@ -44,40 +44,76 @@ var Drink = function(name, description, price) {
 	this.price = price;
 	this.description = description;
 	this.ingredients = [];
-	this.toString = toString;
 };
-
+Drink.prototype.toString = toString;
+// -----------------PLATE---------------------------
 var Plate = function(name, description, price) {
 	this.name = name;
 	this.price = price;
 	this.description = description;
 	this.ingredients = [];
-	this.toString = toString;
+};
+	// Plate methods
+Plate.prototype.toString = toString;
+
+Plate.prototype.isVegan = function() {
+	var isVegan = true;
+	this.ingredients.map(function(item) {
+		if (item.vegan === false) {
+			return isVegan = false;
+		}
+	});
+	return isVegan;
 };
 
+Plate.prototype.isGlutenFree = function() {
+	var glutenFree = true;
+	this.ingredients.map(function(item) {
+		if (item.glutenFree === false) {
+			return glutenFree = false;
+		}
+	});
+	return glutenFree;
+};
+
+Plate.prototype.isCitrusFree = function() {
+	var citrusFree = true;
+	this.ingredients.map( function(item) {
+		if (item.citrusFree === false) {
+			return citrusFree = false;
+		}
+	});
+	return citrusFree;
+};
+
+// ----------------ORDER-------------------------
 var Order = function() {
 	this.plates = [];
-	this.toString = toString;
 };
+Order.prototype.toString = toString;
 
+// -----------------MENU-------------------------
 var Menu = function() {
 	this.plates = [];
-	this.toString = toString;
 };
+Menu.prototype.toString = toString;
 
+// -----------------RESTAURANT-------------------------
 var Restaurant = function(name, description, menu) {
 	this.name = name;
 	this.description = description;
 	this.menu = menu;
-	this.toString = toString;
 };
+Restaurant.prototype.toString = toString;
 
+// -----------------CUSTOMER-------------------------
 var Customer = function(dietaryPreference) {
 	this.dietaryPreference = dietaryPreference;
-	this.toString = toString;
 };
+Customer.prototype.toString = toString;
 
 
+// -----------------TO STRING------------------
 function toString() {
 	var message = '';
 	for (key in this){
@@ -90,7 +126,49 @@ function toString() {
 
 
 
-var beer = new Drink('beer', 'alcohol', 3);
+// Create new instances of FoodItems
+var tortilla = new FoodItem('tortilla',100,true,false,true);
+var beans = new FoodItem('beans',100,true,true,true);
+var rice = new FoodItem('rice',100,true,false,true);
+var steak = new FoodItem('steak',200,false,true,true);
+var lime = new FoodItem('lime',5,true,true,false);
+var avacado = new FoodItem('avacado',10,true,true,true);
+var greenPeppers = new FoodItem('Green Peppers',5,true,true,true);
+var tequila = new FoodItem('tequila', 100,true,true,true);
+var salt = new FoodItem('salt',0,true,true,true);
+var margaritaMix = new FoodItem('Margarita Mix',200,true,true,false);
+
+// Create new instances of Plates
+var burrito = new Plate('Burrito Plate','Burrito on a plate',7);
+var guacamole = new Plate('Guacamole Plate','Guacamole!!!',5);
+// Add Ingredients to Plates
+burrito.ingredients = [tortilla, beans, rice, steak, lime];
+guacamole.ingredients = [avacado, lime, greenPeppers];
+
+// Create new instances of Drinks
+var margarita = new Drink('Margarita','booze',3);
+// Add ingredients to Drinks
+margarita.ingredients = [tequila, salt, margaritaMix];
+
+// Create new menu
+var ourMenu = new Menu();
+// Add plates to menu
+ourMenu.plates = [burrito, guacamole, margarita];
+
+// Create new Restaurant
+var ourRestaurant = new Restaurant("Pepe's",'Mexican Food',ourMenu);
+
+
+
+
+
+console.log(ourRestaurant.toString());
+
+
+
+// console.log(burrito.isVegan());
+// console.log(burrito.isGlutenFree());
+// console.log(burrito.isCitrusFree());
 
 
 
